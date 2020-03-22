9ae0a34125102f6666cac95c201b3f2f37e26a48
Semimartian <semimartian@hotmail.com>
2020-03-22 03:55:02 +0200

    UI changes, cute animations

---
+++ Assets/WinPopUpStar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPopUpStar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject star;
+    public void FillStar()
+    {
+        star.SetActive(true);
+        star.GetComponent<Animator>().SetTrigger("FillStar");
+    }
+    public void EmptyStar()
+    {
+        star.SetActive(false);
+    }
+}
