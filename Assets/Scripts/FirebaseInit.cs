ace978ba35b661ba2b6c3a1161830ab85220aa02
Gal <gallib2@gmail.com>
2020-05-23 16:45:29 +0300

    firebase and GA

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,14 @@
+﻿using Firebase;
+using Firebase.Analytics;
+using UnityEngine;
+
+public class FirebaseInit : MonoBehaviour
+{
+    private void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+}
