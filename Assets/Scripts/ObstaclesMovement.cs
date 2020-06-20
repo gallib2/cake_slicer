6d2d7f8b93f1aed3cf6d2d0f2ad1d23a2dc3d912
Gal <gallib2@gmail.com>
2020-06-20 19:39:54 +0300

    add obstacles movement

---
+++ Assets/Scripts/ObstaclesMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesMovement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    PolygonCollider2D poly;
+    private bool changeDirection = true;
+    private bool toMoveHorizontal;
+
+    void Start()
+    {
+        toMoveHorizontal = Random.value < 0.5;
+    }
+
+    void Update()
+    {
+        poly = transform.parent.GetComponent<PolygonCollider2D>();
+        bool isPositionInsideParentBounds = poly.bounds.Contains(transform.localPosition);
+        if (!isPositionInsideParentBounds)
+        {
+            changeDirection = !changeDirection;
+            //direction *= -1;
+
+        }
+
+        if (changeDirection)
+        {
+            if(toMoveHorizontal)
+            {
+                MoveHorizontal(1);
+            }
+            else
+            {
+                MoveVertical(1);
+            }
+        }
+        else
+        {
+            if (toMoveHorizontal)
+            {
+                MoveHorizontal(-1);
+            }
+            else
+            {
+                MoveVertical(-1);
+            }
+        }
+    }
+
+    private void MoveHorizontal(int direction)
+    {
+        transform.position += direction * transform.right * speed * Time.deltaTime;
+    }
+
+    private void MoveVertical(int direction)
+    {
+        transform.position += direction * transform.up * speed * Time.deltaTime;
+    }
+}
